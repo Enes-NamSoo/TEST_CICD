4952845a6345b20acb8e457b32066818cca3b2f7
namsoo5 <are34@naver.com>
2022-10-28 17:36:59 +0900

    :sparkles: 프로젝트생성

---
+++ test_CICD/test_CICD/test_CICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_CICDApp.swift
+//  test_CICD
+//
+//  Created by 김남수 on 2022/10/28.
+//
+
+import SwiftUI
+
+@main
+struct test_CICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
