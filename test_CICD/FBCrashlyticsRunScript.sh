f61299104e3728be16d28b9587dbe94b07ae8692
enes <enes@zuminternet.com>
2022-11-11 15:13:40 +0900

    google service 경로수정

--- test_CICD/FBCrashlyticsRunScript.sh
+++ test_CICD/FBCrashlyticsRunScript.sh
@@ -3,13 +3,19 @@
 #!/bin/sh
 echo "🟢🟢🟢🟢🟢🟢🟢🟢🟢"
 
+if [ "${CONFIGURATION}" == "Release" ]; then
+echo "🟢release!!!!"
+else
+echo "🟢debug!!!!" "${CONFIGURATION}"
+fi
+
 
 #set -e // 오류발생시 종료하는 옵션
 if [[ -n $CI_ARCHIVE_PATH ]]; then
 echo "Found valid archive path, trying to upload dSYMs."
 
     "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run"
-    "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "../${CI_PRODUCT}/GoogleService-Info.plist" -p ios "${CI_ARCHIVE_PATH}/dSYMs"
+    "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "${PROJECT_DIR}/test_CICD/GoogleService-Info.plist" -p ios "${CI_ARCHIVE_PATH}/dSYMs"
 else
     echo "firebase crashlytics debug run..."
     "${BUILD_DIR%Build/*}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run"
