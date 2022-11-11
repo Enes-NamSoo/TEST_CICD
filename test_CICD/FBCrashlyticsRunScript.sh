13bf426f58a7bcdbe38a6994a4464a8ee97cbce6
enes <enes@zuminternet.com>
2022-11-11 16:25:20 +0900

    경로수정

--- test_CICD/FBCrashlyticsRunScript.sh
+++ test_CICD/FBCrashlyticsRunScript.sh
@@ -2,19 +2,14 @@
 
 #!/bin/sh
 echo "🟢🟢🟢🟢🟢🟢🟢🟢🟢"
-
-if [ "${CONFIGURATION}" == "Release" ]; then
-echo "🟢release!!!!"
-else
-echo "🟢debug!!!!" "${CONFIGURATION}"
-fi
+echo "🟢${CONFIGURATION}🟢"
 
 set -e
 
 if [[ -n $CI_ARCHIVE_PATH ]]; then
     echo "upload dSYMs..."
     "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run"
-    "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "${PROJECT_DIR}/test_CICD/GoogleService-Info.plist" -p ios "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}"
+    "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "${PROJECT_DIR}/test_CICD/GoogleService-Info.plist" -p ios "$CI_ARCHIVE_PATH/dSYMs"
 else
     echo "firebase crashlytics debug run..."
     "${BUILD_DIR%Build/*}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run"
