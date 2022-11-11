2ce104556bdbf016f02c926f78b754a32393a284
enes <enes@zuminternet.com>
2022-11-11 16:12:37 +0900

    텍스트변경

--- test_CICD/FBCrashlyticsRunScript.sh
+++ test_CICD/FBCrashlyticsRunScript.sh
@@ -9,10 +9,10 @@ else
 echo "🟢debug!!!!" "${CONFIGURATION}"
 fi
 
-#set -e // 오류발생시 종료하는 옵션
-if [[ -n $CI_ARCHIVE_PATH ]]; then
-echo "Found valid archive path, trying to upload dSYMs."
+set -e
 
+if [[ -n $CI_ARCHIVE_PATH ]]; then
+    echo "upload dSYMs..."
     "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run"
     "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "${PROJECT_DIR}/test_CICD/GoogleService-Info.plist" -p ios "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}"
 else
