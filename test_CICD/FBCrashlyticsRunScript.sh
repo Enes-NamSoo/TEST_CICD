7e1735a70cb348aaaba069b20cbb7202afd49db7
enes <enes@zuminternet.com>
2022-11-11 17:50:05 +0900

    코드정리

--- test_CICD/FBCrashlyticsRunScript.sh
+++ test_CICD/FBCrashlyticsRunScript.sh
@@ -4,25 +4,19 @@
 echo "🟢🟢🟢🟢🟢🟢🟢🟢🟢"
 echo "🟢${CONFIGURATION}🟢"
 
-echo "${BUILD_DIR%Build/*}"
+echo "path test..."
 echo "🟢${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}🟢"
 echo "🟢${CI_DERIVED_DATA_PATH}/Build/Intermediates.noindex/ArchiveIntermediates/test_CICD/BuildProductsPath/Release-iphoneos/${DWARF_DSYM_FILE_NAME}🟢"
+echo "🟢${BUILD_DIR%Build/*}🟢"
 
-set -e
-
-if [[ -n $CI_ARCHIVE_PATH ]]; then
-    echo "upload dSYMs..."
-    if [ -d ${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME} ]; then
-        "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run"
-        "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "${PROJECT_DIR}/test_CICD/GoogleService-Info.plist" -p ios "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}"
-    else
-        echo "dSYMs is not found"
-    fi
-else
-    echo "firebase crashlytics debug run..."
+echo "upload dSYMs..."
+if [ -d ${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME} ]; then
     "${BUILD_DIR%Build/*}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run"
     "${BUILD_DIR%Build/*}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "${PROJECT_DIR}/test_CICD/GoogleService-Info.plist" -p ios "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}"
+else
+    echo "dSYMs is not found"
 fi
+
 
 echo "finish crashlytics run"
 echo "🟢🟢🟢🟢🟢🟢🟢🟢🟢"
