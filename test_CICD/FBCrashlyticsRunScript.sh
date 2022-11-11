b3e7edf5c5b7058d7ff2b35ec0c9e12762aaa14c
enes <enes@zuminternet.com>
2022-11-11 17:36:19 +0900

    오타수정

--- test_CICD/FBCrashlyticsRunScript.sh
+++ test_CICD/FBCrashlyticsRunScript.sh
@@ -4,6 +4,7 @@
 echo "🟢🟢🟢🟢🟢🟢🟢🟢🟢"
 echo "🟢${CONFIGURATION}🟢"
 
+echo "${BUILD_DIR%Build/*}"
 echo "🟢${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}🟢"
 echo "🟢${CI_DERIVED_DATA_PATH}/Build/Intermediates.noindex/ArchiveIntermediates/test_CICD/BuildProductsPath/Release-iphoneos/${DWARF_DSYM_FILE_NAME}🟢"
 
@@ -11,7 +12,7 @@ set -e
 
 if [[ -n $CI_ARCHIVE_PATH ]]; then
     echo "upload dSYMs..."
-    if [ -d ${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME} ] then
+    if [ -d ${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME} ]; then
         "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run"
         "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "${PROJECT_DIR}/test_CICD/GoogleService-Info.plist" -p ios "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}"
     else
