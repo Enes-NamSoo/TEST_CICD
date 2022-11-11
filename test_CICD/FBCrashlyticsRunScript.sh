79cd27b625642aa4f37af6117357093a446a47c8
enes <enes@zuminternet.com>
2022-11-11 14:03:36 +0900

    경로수정

--- test_CICD/FBCrashlyticsRunScript.sh
+++ test_CICD/FBCrashlyticsRunScript.sh
@@ -3,29 +3,24 @@
 #!/bin/sh
 echo "🟢🟢🟢🟢🟢🟢🟢🟢🟢"
 
+ls
+
+echo "🟢 find... derived path "
+find "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk" upload-symbols -type
+
+
 #set -e // 오류발생시 종료하는 옵션
 if [[ -n $CI_ARCHIVE_PATH ]];
 then
 echo "Found valid archive path, trying to upload dSYMs."
 
-if [ "${CONFIGURATION}" == "RELEASE" ]; then
-echo "firebase crashlytics Release run..."
-chmod +x "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run"
-chmod +x "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "./${CI_PRODUCT}/${CI_PRODUCT}/GoogleService-Info.plist" -p ios "${CI_ARCHIVE_PATH}/dSYMs"
-
-"${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run"
-"${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "./${CI_PRODUCT}/${CI_PRODUCT}/GoogleService-Info.plist" -p ios "${CI_ARCHIVE_PATH}/dSYMs"
-
-else
-
 echo "firebase crashlytics debug run..."
 chmod +x "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run"
-chmod +x "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "./${CI_PRODUCT}/${CI_PRODUCT}/GoogleService-Info.plist" -p ios "${CI_ARCHIVE_PATH}/dSYMs"
+chmod +x "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "./${CI_PRODUCT}/GoogleService-Info.plist" -p ios "${CI_ARCHIVE_PATH}/dSYMs"
 
 "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run"
-"${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "./${CI_PRODUCT}/${CI_PRODUCT}/GoogleService-Info.plist" -p ios "${CI_ARCHIVE_PATH}/dSYMs"
+"${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "./${CI_PRODUCT}/GoogleService-Info.plist" -p ios "${CI_ARCHIVE_PATH}/dSYMs"
 
-fi
 fi
 
 echo "finish crashlytics run"
