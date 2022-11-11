676fa8dd4fc0bf2cb6d5e8e987c77437d94678d9
enes <enes@zuminternet.com>
2022-11-11 14:24:21 +0900

    경로수정

--- test_CICD/FBCrashlyticsRunScript.sh
+++ test_CICD/FBCrashlyticsRunScript.sh
@@ -3,10 +3,10 @@
 #!/bin/sh
 echo "🟢🟢🟢🟢🟢🟢🟢🟢🟢"
 
-ls
+echo "${CONFIGURATION}"
 
 echo "🟢 find... derived path "
-find "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk" upload-symbols -type
+find "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk" upload-symbols
 
 
 #set -e // 오류발생시 종료하는 옵션
@@ -16,10 +16,10 @@ echo "Found valid archive path, trying to upload dSYMs."
 
 echo "firebase crashlytics debug run..."
 chmod +x "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run"
-chmod +x "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "./${CI_PRODUCT}/GoogleService-Info.plist" -p ios "${CI_ARCHIVE_PATH}/dSYMs"
+chmod +x "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "../${CI_PRODUCT}/GoogleService-Info.plist" -p ios "${CI_ARCHIVE_PATH}/dSYMs"
 
 "${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run"
-"${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "./${CI_PRODUCT}/GoogleService-Info.plist" -p ios "${CI_ARCHIVE_PATH}/dSYMs"
+"${CI_DERIVED_DATA_PATH}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" -gsp "../${CI_PRODUCT}/GoogleService-Info.plist" -p ios "${CI_ARCHIVE_PATH}/dSYMs"
 
 fi
 
